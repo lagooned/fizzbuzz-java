bd2eb75ca819daafbe6daee269554c966a7a872d
Jared Engler <jared.lite@gmail.com>
2020-09-22 01:06:27 -0500

    add size n base case test

--- src/main/java/com/jaredengler/FizzBuzzer.java
+++ src/main/java/com/jaredengler/FizzBuzzer.java
@@ -2,11 +2,20 @@
 package com.jaredengler;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class FizzBuzzer {
 
     List<String> fizzBuzz(Integer n) {
-        return List.of();
+
+        if (null == n)
+            return List.of();
+
+        return IntStream.range(0, n).boxed()
+            .map(Object::toString)
+            .collect(Collectors.toList());
+
     }
 
 }
--- src/test/java/com/jaredengler/FizzBuzzerShould.java
+++ src/test/java/com/jaredengler/FizzBuzzerShould.java
@@ -7,6 +7,7 @@ import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -28,6 +29,11 @@ public class FizzBuzzerShould {
     @Test
     void return_empty_list_when_0() {
         assertThat(fizzBuzzer.fizzBuzz(0).isEmpty(), is(true));
+    }
+
+    @Test
+    void return_list_with_size_n() {
+        assertThat(fizzBuzzer.fizzBuzz(16).size(), is(equalTo(16)));
     }
 
 }
